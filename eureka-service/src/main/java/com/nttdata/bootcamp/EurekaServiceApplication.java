96ae60dea8877e82cd57299a999c1247c4f4b1aa
Fran Ferreras <franferreraszl@gmail.com>
2022-10-05 13:46:03 +0200

    Eureka-service

--- eureka-service/src/main/java/com/nttdata/bootcamp/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/nttdata/bootcamp/EurekaServiceApplication.java
@@ -2,14 +2,14 @@ package com.nttdata.bootcamp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableConfigServer
-public class ConfigServiceApplication {
+@EnableEurekaServer
+public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServiceApplication.class, args);
+		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
 
 }
